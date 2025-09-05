2673e1bd086b5772d2dd4590a4a3a74b1ec689f3
Faizan Rajab <faizanrajabali@outlook.com>
2025-09-05 19:52:02 +0500

    APIUrlUpdated

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export const CONFIG = {
     // This will fall back to the local URL if VITE_API_BASE_URL is not set.
     // This ensures that your local dev environment always works without needing
     // to set an environment variable on your machine.
-    BASE_API_URL: import.meta.env.VITE_API_BASE_URL || 'https://localhost:7266',
+    BASE_API_URL: import.meta.env.VITE_API_BASE_URL || 'https://hmapi.somee.com/',
     R2_BASE_URL: 'https://pub-867806bcf0914258bb29ce36f455004f.r2.dev',
     DUMMY_IMAGE_URL: 'https://themindfulaimanifesto.org/wp-content/uploads/2020/09/male-placeholder-image.jpeg',
     currencySymbol: 'Rs',
